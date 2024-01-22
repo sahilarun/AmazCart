5735304bba20ff5c51210043952f2053cf83710b
Sahil <151955025+sahilarun@users.noreply.github.com>
2024-01-22 23:22:30 +0530

    ios #1

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\sahil\Music\codecanyon-35656743-flutter-amazcart-ecommerce-flutter-source-code-for-android-and-iso\Amazcart_customer_flutter_package_v3.0\amazcart_app"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.8.0"
+export "FLUTTER_BUILD_NUMBER=31"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
